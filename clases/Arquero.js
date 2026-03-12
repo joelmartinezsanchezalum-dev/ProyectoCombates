301634cb844538c985eabb1c24d60bdc7badd17a
danigv23 <danigarciavico@gmail.com>
2026-03-12 16:38:57 +0100

    crear arquero

---
+++ clases/Arquero.js
@@ -0,0 +1,13 @@
+const { Personaje } = require("./Personaje.js");
+
+class Arquero extends Personaje {
+
+    constructor() {
+        super();
+
+        // this.vida = 50;
+        // this.#poder = 50;
+        // this.#velocidad = 50;
+    }
+
+}
