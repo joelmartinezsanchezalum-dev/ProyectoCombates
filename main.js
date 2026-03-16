c094e3ef08168b9f6c81f385a048635661fe0797
Adrian <adrian.mihalache11@gmail.com>
2026-03-16 17:15:33 +0100

    Añadido main a rama adri

---
+++ main.js
@@ -0,0 +1,16 @@
+const { Paladin } = require("./clases")
+
+const prompt = require("prompt-sync")({ sigint: true })
+
+// Menu principal 
+function mostrarMenu() {
+    console.log("Hola jugador, que personaje deseas ser? ")
+    console.log("1- Paladin Humano")
+    console.log("2- Mago Elfo ")
+    console.log("3- Guerrero Enano")
+    console.log("4- Arquero Mediano")
+};
+
+let fsdf = new Paladin();
+
+console.log(fsdf.poder);
