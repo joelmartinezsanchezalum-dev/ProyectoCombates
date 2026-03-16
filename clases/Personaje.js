a4bd2ec46ad6e44383f37361e56a45f1c3c95008
Adrian <adrian.mihalache11@gmail.com>
2026-03-16 16:37:52 +0100

    Added updated Personaje to branch adri

--- clases/Personaje.js
+++ clases/Personaje.js
@@ -1,12 +1,53 @@
 class Personaje {
-    vida
-    poder
-    velocidad
 
-    nameAtaque1
-    nameAtaque2
+    #vida
+    #poder
+    #velocidad
+
+    #nameAtaque1
+    #nameAtaque2
 
     constructor() { }
+
+    get vida() {
+        return this.#vida;
+    };
+
+    get poder() {
+        return this.#poder;
+    };
+
+    get velocidad() {
+        return this.#velocidad;
+    };
+
+    get nameAtaque1() {
+        return this.#nameAtaque1;
+    };
+
+    get nameAtaque2() {
+        return this.#nameAtaque2;
+    };
+
+    set vida(vida) {
+        this.#vida = vida;
+    }
+
+    set poder(poder) {
+        this.#poder = poder;
+    }
+
+    set velocidad(velocidad) {
+        this.#velocidad = velocidad;
+    }
+
+    set nameAtaque1(nameAtaque1) {
+        this.#nameAtaque1 = nameAtaque1;
+    }
+
+    set nameAtaque2(nameAtaque2) {
+        this.#nameAtaque2 = nameAtaque2;
+    }
 
     ataque1() { }
     ataque2() { }
